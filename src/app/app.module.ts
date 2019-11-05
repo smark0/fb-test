39fe6f64b6745269c8a825077a37d82f3f1966ec
Steven Markhardt <steven.markhardt@gmx.at>
2019-11-05 14:14:07 +0100

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "angularfire2"
+import { AngularFireDatabaseModule} from "angularfire2/database"
+import { environment } from '../environments/environment';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerService } from "./shared/customer.service";
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),// we called initializeApp function to provide connection details
+    AngularFireDatabaseModule // we will import the classes here too
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCco5pUHqmwPda8_JiWlf5WkWonqD9Kkzg",
+    authDomain: "business-bca56.firebaseapp.com",
+    databaseURL: "https://business-bca56.firebaseio.com",
+    projectId: "business-bca56",
+    storageBucket: "business-bca56.appspot.com",
+    messagingSenderId: "1016344077243",
+    appId: "1:1016344077243:web:8550b0ec703e1c893f5a9e",
+    measurementId: "G-E03FPD06W4"
+},};
 
 /*
  * For easier debugging in development mode, you can import the following file
